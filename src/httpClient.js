972cc019d824c67fc231e6bccd68b7e900fd36d2
yashnaik113-collab <yashnaik113@gmail.com>
2026-03-31 20:27:29 +0530

    feat:rendering website on render

--- src/httpClient.js
+++ src/httpClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const httpClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
